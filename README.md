27a3578065755db008e233bdc79861a9bf0c651d
Philippe Gagné <phgag@icloud.com>
2024-01-06 22:08:38 -0500

    Temperature and humidity read and transmitted to bthome.

--- README.md
+++ README.md
@@ -2,15 +2,27 @@
 This is a door alarm for a fridge.
 
 Uses a custom STM32WB5MMGH6TR board with the Zephyr SDK platform.
+We transmit the state of this iot device using the BTHome protocol (<https://bthome.io/format/>).
+
+Ref: <https://docs.zephyrproject.org/latest/boards/arm/nucleo_wb55rg/doc/nucleo_wb55rg.html>
 
 ## Preparation
 You must flash the binary bluetooth stack code into the MCU (<https://wiki.st.com/stm32mcu/wiki/Connectivity:STM32WB_BLE_Hardware_Setup>) using STM32CubeProgrammer utility.
 
-For the present version, we need to flash
-STM32CubeWB/Projects/STM32WB_Copro_Wireless_Binaries/STM32WB5x/stm32wb5x_BLE_Stack_full_fw.bin
-The start address is 0x080CE000 for our MCU.
+For the present version, we need to flash (Zephyr limit which BT stack we can use):
+```STM32CubeWB/Projects/STM32WB_Copro_Wireless_Binaries/STM32WB5x/stm32wb5x_BLE_HCILayer_fw.bin```
+The start address is 0x080E0000 for our MCU.
 
 To get the address value, look into STM32CubeWB/Projects/STM32WB_Copro_Wireless_Binaries/STM32WB5x/Release_Notes.html
 
 ## Compile
 We use zephyr SDK (<https://www.zephyrproject.org>) to compile the source.
+An introduction: <https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/zephyr/develop/west/basics.html>.
+
+Activate the west environement, then in the Zephyr directory, 
+
+    cd workspace
+    west init
+    west update
+    west build -b nucleo_wb55rg -p always ../zephyr/
+    west -z /Volumes/External/zephyrproject/zephyr  build -b nucleo_wb55rg -p always ../zephyr/
--- zephyr/src/main.c
+++ zephyr/src/main.c
@@ -17,34 +17,77 @@
 #include <zephyr/bluetooth/gap.h>
 #include <zephyr/bluetooth/uuid.h>
 
-#define SERVICE_DATA_LEN 9
-#define SERVICE_UUID 0xfcd2 /* BTHome service UUID */
-#define IDX_TEMPL 4         /* Index of lo byte of temp in service data*/
-#define IDX_TEMPH 5         /* Index of hi byte of temp in service data*/
+/*
+ ***************************************************************************
+ * Device tree definitions
+ */
 
-// Example
 // https://github.com/aaron-mohtar-co/Lemon-IoT-LTE-nRF9160/tree/main/Examples/i2c_sensor
 // #define OPT3001_DEV DT_NODELABEL(opt3001)
-// #define SHTC_DEV DT_NODELABEL(shtcx)
+#define SHTC_DEV DT_NODELABEL(shtcx)
 #define DOOR DT_ALIAS(sw0)
 #define CTRL DT_ALIAS(sw1)
 #define LED0 DT_ALIAS(led0)
 #define BUZZER DT_ALIAS(led1)
-
-#define ADV_PARAMS                                                             \
-  (BT_LE_ADV_PARAM(BT_LE_ADV_OPT_USE_NAME, BT_GAP_ADV_FAST_INT_MIN_2,          \
-                   BT_GAP_ADV_FAST_INT_MAX_2, NULL))
 
 // DeviceTree devices
 const struct gpio_dt_spec buzzer = GPIO_DT_SPEC_GET(BUZZER, gpios);
 const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0, gpios);
 const struct gpio_dt_spec door_sw = GPIO_DT_SPEC_GET(DOOR, gpios);
 const struct gpio_dt_spec ctrl_btn = GPIO_DT_SPEC_GET(CTRL, gpios);
-// const struct device *light = DEVICE_DT_GET(OPT3001_DEV);
-// const struct device *sht = DEVICE_DT_GET(SHTC_DEV);
+//const struct device *light = DEVICE_DT_GET(OPT3001_DEV);
+const struct device *sht = DEVICE_DT_GET(SHTC_DEV);
 
 static struct gpio_callback door_cb;
 static struct gpio_callback ctrl_cb;
+
+/*
+ ***************************************************************************
+ * Bluetooth BtHome setup.
+ ****************************************************************************
+ */
+#define SERVICE_DATA_LEN 9
+#define SERVICE_UUID 0xfcd2 /* BTHome service UUID */
+#define IDX_TEMPL 4         /* Index of lo byte of temp in service data*/
+#define IDX_TEMPH 5         /* Index of hi byte of temp in service data*/
+#define IDX_HUMDL 7         /* Index of lo byte of humidity in service data*/
+#define IDX_HUMDH 8         /* Index of hi byte of humidity in service data*/
+
+static uint8_t service_data[SERVICE_DATA_LEN] = {
+    BT_UUID_16_ENCODE(SERVICE_UUID),
+    0x40,
+    0x02, /* Temperature */
+    0xc4, /* Low byte */
+    0x00, /* High byte */
+    0x03, /* Humidity */
+    0xbf, /* 50.55%  low byte*/
+    0x13, /* 50.55%  high byte*/
+};
+
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR),
+    BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME,
+            sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+   BT_DATA(BT_DATA_SVC_DATA16,
+            service_data, ARRAY_SIZE(service_data)),
+};
+
+/*
+ #define ADV_PARAMS                                                             \
+   BT_LE_ADV_PARAM(BT_LE_ADV_OPT_USE_NAME, BT_GAP_ADV_FAST_INT_MIN_2,          \
+                    BT_GAP_ADV_FAST_INT_MAX_2, NULL)
+*/
+
+#define ADV_PARAMS BT_LE_ADV_PARAM(BT_LE_ADV_OPT_USE_IDENTITY, \
+				  BT_GAP_ADV_SLOW_INT_MIN, \
+				  BT_GAP_ADV_SLOW_INT_MAX, NULL)
+
+
+/*
+ ***************************************************************************
+ * Door alarm setup.
+ ****************************************************************************
+ */
 
 // TODO this code is disgusting, software debounce the buttons
 // https://github.com/ubieda/zephyr_button_debouncing/tree/master
@@ -101,25 +144,12 @@ void door_state_change(const struct device *dev, struct gpio_callback *cb,
   }
 }
 
-static uint8_t service_data[SERVICE_DATA_LEN] = {
-    BT_UUID_16_ENCODE(SERVICE_UUID),
-    0x40,
-    0x02, /* Temperature */
-    0xc4, /* Low byte */
-    0x00, /* High byte */
-    0x03, /* Humidity */
-    0xbf, /* 50.55%  low byte*/
-    0x13, /* 50.55%  high byte*/
-};
 
-static const struct bt_data ad[] = {
-    BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR),
-    BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME,
-            sizeof(CONFIG_BT_DEVICE_NAME) - 1),
-    // BT_DATA(BT_DATA_SVC_DATA16, struct sensor_value temp, hum;
-    //         service_data, ARRAY_SIZE(service_data))};
-};
-
+/*
+ ***************************************************************************
+ * Bluetooth functions.
+ ****************************************************************************
+ */
 static int bt_ready() {
 
   printk("Bluetooth initialized\n");
@@ -129,79 +159,139 @@ static int bt_ready() {
   if (err) {
     printk("Advertising failed to start (err %d)\n", err);
   }
+
+  printk("Advertising started.\n");
   return err;
 }
 
+/*
+ ***************************************************************************
+ * Main
+ ****************************************************************************
+ */
 int main(void) {
   int err;
-  int temp = 0;
 
-  // ----- Init Door Switch + IRQ -----
-  err = gpio_pin_configure_dt(&door_sw, GPIO_INPUT);
-  err = gpio_pin_interrupt_configure_dt(&door_sw, GPIO_INT_EDGE_BOTH);
-  if (err != 0) {
-    printk("Error %d: failed to configure interrupt on %s pin %d\n", err,
-           door_sw.port->name, door_sw.pin);
-    return err;
-  }
-  gpio_init_callback(&door_cb, door_state_change, BIT(door_sw.pin));
-  gpio_add_callback(door_sw.port, &door_cb);
+  printk("Hello !\n");
+
+  // // ----- Init Door Switch + IRQ -----
+  // err = gpio_pin_configure_dt(&door_sw, GPIO_INPUT);
+  // err = gpio_pin_interrupt_configure_dt(&door_sw, GPIO_INT_EDGE_BOTH);
+  // if (err != 0) {
+  //   printk("Error %d: failed to configure interrupt on %s pin %d\n", err,
+  //          door_sw.port->name, door_sw.pin);
+  //   return err;
+  // }
+  // gpio_init_callback(&door_cb, door_state_change, BIT(door_sw.pin));
+  // gpio_add_callback(door_sw.port, &door_cb);
 
   // ----- Init CTRL Button + IRQ -----
-  err = gpio_pin_configure_dt(&ctrl_btn, GPIO_INPUT | GPIO_PULL_DOWN);
-  err = gpio_pin_interrupt_configure_dt(&ctrl_btn, GPIO_INT_EDGE_BOTH);
-  if (err != 0) {
-    printk("Error %d: failed to configure interrupt on %s pin %d\n", err,
-           ctrl_btn.port->name, ctrl_btn.pin);
-    return err;
-  }
-  gpio_init_callback(&ctrl_cb, ctrl_button_pressed, BIT(ctrl_btn.pin));
-  gpio_add_callback(ctrl_btn.port, &ctrl_cb);
-
+  // err = gpio_pin_configure_dt(&ctrl_btn, GPIO_INPUT | GPIO_PULL_DOWN);
+  // err = gpio_pin_interrupt_configure_dt(&ctrl_btn, GPIO_INT_EDGE_BOTH);
+  // if (err != 0) {
+  //   printk("Error %d: failed to configure interrupt on %s pin %d\n", err,
+  //          ctrl_btn.port->name, ctrl_btn.pin);
+  //   return err;
+  // }
+  // gpio_init_callback(&ctrl_cb, ctrl_button_pressed, BIT(ctrl_btn.pin));
+  // gpio_add_callback(ctrl_btn.port, &ctrl_cb);
+ 
   // ----- Init Buzzer -----
   gpio_pin_configure_dt(&buzzer, GPIO_OUTPUT);
   gpio_pin_set_dt(&buzzer, 1);
-  k_msleep(1000);
+  k_msleep(100);
   gpio_pin_set_dt(&buzzer, 0);
+  k_msleep(100);
 
   // ----- Init LED -----
   gpio_pin_configure_dt(&led, GPIO_OUTPUT);
 
   // ----- SHT init -----
-  // if (!device_is_ready(sht)) {
-  //  printf("Device %s is not ready\n", sht->name);
-  // return 0;
-  //} else {
-  //  printk("Found device %s. Reading sensor data\n", sht->name);
-  //}
+  if (!device_is_ready(sht)) {
+   printk("Device %s is not ready\n", sht->name);
 
-  while (1) {
-    ;
+  } else {
+   printk("Found device %s. Reading sensor data\n", sht->name);
   }
+
+  
+  // while (1) {
+  //   ;
+  // }
   // ----- Init BLE -----
-  printk("%s\n", CONFIG_BT_DEVICE_NAME);
+ 
+  printk("CONFIG_BT_DEVICE_NAME: %s\n", CONFIG_BT_DEVICE_NAME);
   err = bt_enable(NULL);
   if (err) {
     printk("Bluetooth init failed (err %d)\n", err);
-    return 0;
-  }
-  err = bt_ready();
-  if (err) {
-    printk("Bluetooth ready failed (err %d)\n", err);
+
+    for (int i=0; i<5; ++i){
+      gpio_pin_set_dt(&buzzer, 1);
+      k_msleep(100);
+      gpio_pin_set_dt(&buzzer, 0);
+      k_msleep(1000);
+    }
+
     return 0;
   }
 
+  printk("Bluetooth init done.\n");
+
+  err = bt_ready();
+  if (err) {
+    printk("Bluetooth ready failed (err %d)\n", err);
+
+    for (int i=0; i<10; ++i){
+      gpio_pin_set_dt(&buzzer, 1);
+      k_msleep(100);
+      gpio_pin_set_dt(&buzzer, 0);
+      k_msleep(1000);
+    } 
+    return 0;
+  }
+
+  printk("Bluetooth ready.\n");
+  
+
   // ----- Main loop -----
   for (;;) {
-    /* Simulate temperature from 0C to 25C */
-    service_data[IDX_TEMPH] = (temp * 100) >> 8;
-    service_data[IDX_TEMPL] = (temp * 100) & 0xff;
-    if (temp++ == 25) {
-      temp = 0;
+
+    /* Get temp & humidity */
+		struct sensor_value temp, hum;
+
+		int err = sensor_sample_fetch(sht);
+		if (err == 0) {
+			err = sensor_channel_get(sht, SENSOR_CHAN_AMBIENT_TEMP,
+						&temp);
+		}
+		if (err == 0) {
+			err = sensor_channel_get(sht, SENSOR_CHAN_HUMIDITY,
+						&hum);
+		}
+		if (err != 0) {
+		  printf("SHT: failed: %d\n", err);
+		}
+    else {
+      double ftemp = sensor_value_to_double(&temp);
+      double fhumd = sensor_value_to_double(&hum);
+      printf("SHT: %.2f Cel ; %0.2f %%RH\n", ftemp, fhumd);
+
+      service_data[IDX_TEMPH] = (int)(ftemp * 100) >> 8;
+      service_data[IDX_TEMPL] = (int)(ftemp * 100) & 0xff;
+
+      service_data[IDX_HUMDH] = (int)(fhumd * 100) >> 8;
+      service_data[IDX_HUMDL] = (int)(fhumd * 100) & 0xff;
     }
+
     err = bt_le_adv_update_data(ad, ARRAY_SIZE(ad), NULL, 0);
     if (err) {
       printk("Failed to update advertising data (err %d)\n", err);
+      for (int i=0; i<20; ++i){
+        gpio_pin_set_dt(&buzzer, 1);
+        k_msleep(100);
+        gpio_pin_set_dt(&buzzer, 0);
+        k_msleep(1000);
+      }
     }
     k_sleep(K_MSEC(BT_GAP_ADV_SLOW_INT_MIN));
   }
